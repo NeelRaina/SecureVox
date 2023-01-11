0c241c17df6babcd529c8fad8a42be57addf123b
Neel Raina <neelraina420@yahoo.com>
2023-01-10 21:32:39 -0500

    Switched to NodeJS to login

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -13,15 +13,17 @@ public class Example {
         post("/", (request, response) -> {
             
             Say say  = new Say.Builder(
-                    "Enter UserID.")
+                    "Enter User")
                     .build();
-            Gather gather = new Gather.Builder().action("/").say(say).build();
+            //Gather gather = new Gather.Builder().action("/").say(say).build();
+            Gather gather1 = new Gather.Builder().build();
             Say say2  = new Say.Builder(
                     "Enter Password.")
                     .build();
             Gather gather2 = new Gather.Builder().action("/").say(say2).build();        
             VoiceResponse voiceResponse = new VoiceResponse.Builder()
-                    .gather(gather)
+                    .say(say)
+                    .gather(gather1)
                     .gather(gather2)
                     .build();
                           
